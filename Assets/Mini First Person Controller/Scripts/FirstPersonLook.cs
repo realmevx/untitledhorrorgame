a1aa64ec5814bc3af40050d3b239ab1384793b10
realmevx <realmevx@gmail.com>
2021-10-01 03:36:54 +0200

    added monster
    rigged and animated

--- Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
+++ Assets/Mini First Person Controller/Scripts/FirstPersonLook.cs
@@ -34,6 +34,6 @@ public class FirstPersonLook : MonoBehaviour
 
         // Rotate camera up-down and controller left-right from velocity.
         transform.localRotation = Quaternion.AngleAxis(-velocity.y, Vector3.right);
-        character.localRotation = Quaternion.AngleAxis(velocity.x, Vector3.up);
+        character.localRotation = Quaternion.AngleAxis(velocity.x - 180, Vector3.up);
     }
 }
